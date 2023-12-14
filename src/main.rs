cdf3617d7c40b480538bdf948b2e47895605d846
lina <me@jackli.dev>
2023-12-14 10:48:37 -0800

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use reqwest::{multipart, Body, Client};
+use std::path::Path;
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
+use clipboard::{ClipboardContext, ClipboardProvider};
+use dotenv::dotenv;
+use std::env::var;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let path = var("DIRECTORY").expect("Expected DIRECTORY in the environment");
+    futures::executor::block_on(async {
+        if let Err(e) = async_watch(path).await {
+            println!("error: {:?}", e)
+        }
+    });
+}
+
+fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+    let watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                tx.send(res).await.unwrap();
+            })
+        },
+        Config::default(),
+    )?;
+
+    Ok((watcher, rx))
+}
+
+async fn async_watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (mut watcher, mut rx) = async_watcher()?;
+    println!("Watching directory: {:?}", path.as_ref());
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => {
+                if event.kind.is_create() {
+                    let original_path = Path::new(&event.paths[0]).to_path_buf();
+                    println!("Detected new file: {:?}", original_path);
+                    let file_name = original_path.file_name().unwrap();
+                    let file_name_str = file_name.to_str().unwrap();
+                    let new_file_name = file_name_str.trim_start_matches(".");
+                    let parent = original_path.parent().unwrap();
+                    let new_path = parent.join(new_file_name);
+                    make_post_request(&new_path).await.unwrap();
+                }
+            }
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+async fn make_post_request(path: &Path) -> reqwest::Result<()> {
+    let client = Client::new();
+    println!("Uploading file: {:?}", path);
+
+    let pathname = Path::new(&path);
+    let filename = pathname.file_name().unwrap();
+    let mime_str = mime_guess::from_path(&path).first().unwrap();
+
+    let file = File::open(path).await.unwrap();
+    let stream = FramedRead::new(file, BytesCodec::new());
+    let file_body = Body::wrap_stream(stream);
+    let some_file = multipart::Part::stream(file_body)
+        .file_name(filename.to_str().unwrap().to_string())
+        .mime_str(mime_str.to_string().as_str())
+        .unwrap();
+    let form = multipart::Form::new().part("file", some_file);
+
+    let link = format!("https://{}/api/upload", var("LINK").expect("Expected LINK in the environment"));
+
+    let response = client
+        .post(link)
+        .header(
+            "Authorization",
+            var("TOKEN").expect("Expected TOKEN in the environment"),
+        )
+        .header("Format", "NAME")
+        .header("Embed", "false")
+        .multipart(form)
+        .send()
+        .await?;
+    let response_body: serde_json::Value = response.json().await?;
+    let url = response_body["files"][0].as_str().unwrap();
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    ctx.set_contents(url.to_string()).unwrap();
+
+    println!("Set clipboard to: {}", url);
+
+    Ok(())
+}
